507f33a19073b53d57d2140943416440b95bd9ec
Jpdl925 <jtpg925@gmail.com>
2024-05-09 21:17:08 -0700

    Added styling

--- counter-app/src/components/Counter.tsx
+++ counter-app/src/components/Counter.tsx
@@ -1,29 +1,36 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Counter = () => {
-    const [counter, setCounter] = useState(0);
-    const increaseCounter = () => {
-        setCounter(counter+1);
+  const [counter, setCounter] = useState(0);
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  const resetCounter = () => {
+    setCounter(0);
+  };
+  const decreaseCounter = () => {
+    if (counter == 0) {
+      console.log("Cannot go below 0");
+    } else {
+      setCounter(counter - 1);
     }
-    const resetCounter = () => {
-        setCounter(0);
-    }
-    const decreaseCounter = () => {
-      if (counter == 0){
-        console.log("Cannot go below 0");
-      }
-      else{
-        setCounter(counter-1);
-      }
-    }
+  };
   return (
     <>
-    <div><h1>Counter {counter}</h1></div>
-    <button onClick={decreaseCounter}>-</button>
-    <button onClick={resetCounter}>RESET</button>
-    <button onClick={increaseCounter}>+</button>
+      <div>
+        <h1>Counter {counter}</h1>
+      </div>
+          <button className="buttonDec" onClick={decreaseCounter}>
+            -
+          </button>
+          <button className="buttonRes" onClick={resetCounter}>
+            RESET
+          </button>
+          <button className="buttonInc" onClick={increaseCounter}>
+            +
+          </button>
     </>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
